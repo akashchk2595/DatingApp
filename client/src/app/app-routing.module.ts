2465217106aa8a5b6d7f6ccca5abeef9e502c721
Akash Chakravorty <akash.chakravorty@siemens-healthineers.com>
2021-06-28 21:53:51 +0530

    Adding Ngx  spinner, persisting cache in UI, CanDeactivate route guard, template forms, member edit functionality

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { PreventUnsavedChangesGuard } from './guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   { path: '', component:HomeComponent },
@@ -19,7 +20,7 @@ const routes: Routes = [
     children: [
       { path: 'members', component:MemberListComponent },
       { path: 'members/:username', component:MemberDetailComponent },
-      { path: 'member/edit', component:MemberEditComponent },
+      { path: 'member/edit', component:MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
       { path: 'lists', component:ListsComponent },
       { path: 'messages', component:MessagesComponent }
       
